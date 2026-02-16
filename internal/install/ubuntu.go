6b33fd83e01b24950d10c25dd6e822e6cd5ed1e0
Gauthier <gbraillon@gmail.com>
2026-02-16 12:55:03 +0100

    feat: implement UbuntuInstaller with validation

---
+++ internal/install/ubuntu.go
@@ -0,0 +1,23 @@
+package install
+
+import "fmt"
+
+type UbuntuInstaller struct{}
+
+func NewUbuntuInstaller() *UbuntuInstaller {
+	return &UbuntuInstaller{}
+}
+
+func (u *UbuntuInstaller) Validate(config Config) error {
+	if config.User.Name == "" {
+		return fmt.Errorf("user name is required")
+	}
+	if config.System.Hostname == "" {
+		return fmt.Errorf("hostname is required")
+	}
+	return nil
+}
+
+func (u *UbuntuInstaller) Install(config Config) error {
+	return fmt.Errorf("not implemented yet")
+}
